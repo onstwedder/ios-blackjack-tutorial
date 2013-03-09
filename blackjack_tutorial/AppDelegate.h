217d7dc554dea4e6964d227b6a5f5d3354bc8616
onstwedder <matijn@onstwedder.com>
2013-03-09 16:07:18 +0100

    initial files
    These are the files as they were at the end of tutorial part 15 (
    http://onstwedder.com/en/Tutorials/Blackjack_Tutorial_for_IOS/ )

---
+++ blackjack_tutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blackjack_tutorial
+//
+//  Created by Martijn Onstwedder on 31-12-12.
+//  Copyright (c) 2012 Onstwedder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blackjack_tutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  blackjack_tutorial
+//
+//  Created by Martijn Onstwedder on 31-12-12.
+//  Copyright (c) 2012 Onstwedder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
